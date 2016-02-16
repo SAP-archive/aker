53f4bd2b726db48a4500ae138f49a84f4dd5a0aa
Momchil Atanassov <momchil.atanassov@sap.com>
2016-02-16 17:23:14 +0200

    Switch to sock connections

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,38 +1,52 @@
 package plugin
 
 import (
-	"io"
+	"net"
 	"os"
 	"os/exec"
-	"os/signal"
-	"syscall"
+	"time"
 
 	"github.wdf.sap.corp/I061150/aker/api"
 	"github.wdf.sap.corp/I061150/aker/connection"
 )
 
 func ListenAndServe(plugin api.Plugin) {
-	peer := connection.NewPeer(os.Stdin, os.Stdout, 1)
-	connection.ServePlugin(peer, plugin)
-	ch := make(chan os.Signal)
-	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
-	for range ch {
-		os.Exit(0)
+	listener, err := net.Listen("unix", "/tmp/aker-plugin.sock")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go func(conn net.Conn) {
+			peer := connection.NewPeer(conn, conn, 1)
+			connection.ServePlugin(peer, plugin)
+		}(conn)
 	}
 }
 
+// FIXME: Only one instance of plugin supported at the moment
 func Open(name string) (api.Plugin, error) {
 	cmd := exec.Command(name)
-	pluginIn, cmdOut := io.Pipe()
-	cmd.Stdout = cmdOut
-	cmdIn, pluginOut := io.Pipe()
-	cmd.Stdin = cmdIn
+	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Start(); err != nil {
 		return nil, err
 	}
 
+	time.Sleep(5 * time.Second) // FIXME
+
+	conn, err := net.Dial("unix", "/tmp/aker-plugin.sock")
+	if err != nil {
+		return nil, err
+	}
+
 	const maxInt = int(^uint(0) >> 1)
-	peer := connection.NewPeer(pluginIn, pluginOut, maxInt/2)
+	peer := connection.NewPeer(conn, conn, maxInt/2)
 	return connection.NewPluginClient(peer), nil
 }
